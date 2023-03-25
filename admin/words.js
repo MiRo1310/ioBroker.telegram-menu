44ae9e88cfb7c630c74bdb88974567985fce20df
MiRo1310 <michael.roling@gmx.de>
2023-03-25 09:58:06 +0100

    First Commit

---
+++ admin/words.js
@@ -0,0 +1,43 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+"use strict";
+
+systemDictionary = {
+	"telegram-menu adapter settings": {
+		en: "Adapter settings for telegram-menu",
+		de: "Adaptereinstellungen für telegram-menu",
+		ru: "Настройки адаптера для telegram-menu",
+		pt: "Configurações do adaptador para telegram-menu",
+		nl: "Adapterinstellingen voor telegram-menu",
+		fr: "Paramètres d'adaptateur pour telegram-menu",
+		it: "Impostazioni dell'adattatore per telegram-menu",
+		es: "Ajustes del adaptador para telegram-menu",
+		pl: "Ustawienia adaptera dla telegram-menu",
+		"zh-cn": "telegram-menu的适配器设置",
+	},
+	option1: {
+		en: "option1",
+		de: "Option 1",
+		ru: "Опция 1",
+		pt: "Opção 1",
+		nl: "Optie 1",
+		fr: "Option 1",
+		it: "opzione 1",
+		es: "Opción 1",
+		pl: "opcja 1",
+		"zh-cn": "选项1",
+	},
+	option2: {
+		en: "option2",
+		de: "Option 2",
+		ru: "вариант 2",
+		pt: "opção 2",
+		nl: "Optie 2",
+		fr: "Option 2",
+		it: "opzione 2",
+		es: "opcion 2",
+		pl: "Opcja 2",
+		"zh-cn": "选项2",
+	},
+};
