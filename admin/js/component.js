3ee410f75c89295c76a20122e3eebfdd5a9c6e8b
MiRo1310 <michael.roling@gmx.de>
2023-04-02 17:34:21 +0200

    global

--- admin/js/component.js
+++ admin/js/component.js
@@ -3,7 +3,7 @@
  * @param {string} activuser Active User
  * @param {array} users List of Users
  */
-function newTableRow(activuser, users) {
+var newTableRow = function (activuser, users) {
 	const userIndex = users.indexOf(activuser);
 	return `<tr>
     <td><input type="text" data-name="call" class="isString nav-call "></td>
@@ -17,22 +17,22 @@ function newTableRow(activuser, users) {
   </p></td>
     <td style="width: 5%;"><a class="delete btn-floating btn-small waves-effect waves-light red"><i	class="material-icons">delete</i></a></td>
             </tr>`;
-}
+};
 /**
  *
  * @param {string} user User Name
  * @returns HTML Element
  */
-function newUserBtn(user) {
+var newUserBtn = function (user) {
 	return `<li class="tab col s1 bg-nav-user"><a name="${user}" class="click_user" href="#">${user}</a></li>`;
-}
+};
 
 /**
  *
  * @param {string} user User Name
  * @returns HTML Element
  */
-function navElement(user) {
+var navElement = function (user) {
 	return `<tbody id="${user}" data-nosave="true" class="table_entry value table-lines table-values">
 							</tbody>`;
-}
+};
