42d788c82e601954dc863731905333fd25e46f75
MiRo1310 <michael.roling@gmx.de>
2023-04-02 11:48:00 +0200

    Html Elemente ausgelagert

---
+++ admin/js/component.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * @param {string} activuser Active User
+ * @param {array} users List of Users
+ */
+function newTableRow(activuser, users) {
+	let userIndex = users.indexOf(activuser);
+	return `<tr>
+    <td><input type="text" data-name="call" class="isString nav-call "></td>
+    <td><input type="text" data-name="value" class="isString nav-value "></td>
+    <td><input type="text" data-name="text" class="isString nav-text "></td>
+    <td><p>
+    <label>
+      <input name="group${userIndex}" type="radio" class="nav-radio" />
+      <span></span>
+    </label>
+  </p></td>
+    <td style="width: 5%;"><a class="delete btn-floating btn-small waves-effect waves-light red"><i	class="material-icons">delete</i></a></td>
+            </tr>`;
+}
+function newUserBtn(user) {
+	return `<li class="tab col s1 bg-nav-user"><a name="${user}" class="click_user" href="#">${user}</a></li>`;
+}
+
+function navElement(user) {
+	return `<tbody id="${user}" data-nosave="true" class="table_entry value table-lines table-values">
+							</tbody>`;
+}
