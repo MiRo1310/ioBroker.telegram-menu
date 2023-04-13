aa208cd40ca30f88ef7e2802a6e20518573dd710
MiRo1310 <michael.roling@gmx.de>
2023-04-13 13:55:58 +0200

    Action implementiert

--- admin/js/component.js
+++ admin/js/component.js
@@ -3,9 +3,9 @@
  * @param {string} activuser Active User
  * @param {array} users List of Users
  */
-var newTableRow = function (activuser, users) {
+var newTableRow_Nav = function (activuser, users) {
 	const userIndex = users.indexOf(activuser);
-	return `<tr>
+	return /*html*/ `<tr>
     <td><input type="text" data-name="call" class="isString nav-call "></td>
     <td><input type="text" data-name="value" class="isString nav-value "></td>
     <td><input type="text" data-name="text" class="isString nav-text "></td>
@@ -15,16 +15,8 @@ var newTableRow = function (activuser, users) {
       <span></span>
     </label>
   </p></td>
-    <td style="width: 5%;"><a class="delete btn-floating btn-small waves-effect waves-light red"><i	class="material-icons">delete</i></a></td>
+    <td style="width: 5%;"><a class="deleteRow btn-floating btn-small waves-effect waves-light red"><i	class="material-icons">delete</i></a></td>
             </tr>`;
-};
-/**
- *
- * @param {string} user User Name
- * @returns HTML Element
- */
-var newUserBtn = function (user) {
-	return `<li class="tab col s1 bg-nav-user"><a name="${user}" class="click_user" href="#">${user}</a></li>`;
 };
 
 /**
@@ -32,7 +24,144 @@ var newUserBtn = function (user) {
  * @param {string} user User Name
  * @returns HTML Element
  */
-var navElement = function (user) {
-	return `<tbody id="${user}" data-nosave="true" class="table_entry value table-lines table-values">
-							</tbody>`;
+var newUserBtn = function (user) {
+	return /*html*/ `<li class="tab col s2 bg-nav-user"><a name="${user}" class="click_user" href="#">${user}</a></li>`;
 };
+
+//TODO - id durch name ersetzen
+/**
+ *
+ * @param {string} user User Name
+ * @returns HTML Element
+ */
+function navElement(user) {
+	return /*html*/ `<tbody id="${user}"name="${user}" data-name="nav" data-nosave="true" class="user_${user} table_switch_user table_entry value table-lines table-values">
+							</tbody>`;
+}
+
+function createSelectTrigger(list) {
+	let element = '<option value="" disabled selected>Choose a trigger</option>';
+	list.forEach(function (e) {
+		let dynamicChild = `<option value="${e}" >${e}</option>`;
+		element += dynamicChild;
+	});
+	return element;
+}
+
+// Row in Action Popup
+function newTrInAction(val) {
+	if (val === "get")
+		return /*html*/ `<tr>
+  <td> <input placeholder="ID" class="get_id" type="text">
+  </td>
+  <td><a class="btn-floating btn-small waves-effect waves-light blue btn_getID"
+    title="Get ID"><i class="material-icons">edit</i></a></td>
+  <td>
+    <input class="get_text" type="text">
+  </td>
+  <td><label>
+      <input type="checkbox" class="filled-in newline_checkbox" checked />
+      <span>Newline</span>
+    </label></td>
+    <td><a class="deleteRow btn-floating btn-small waves-effect waves-light red" ><i
+											class="material-icons">delete</i></a></td>
+								</td>
+</tr>`;
+	if (val === "set")
+		return /*html*/ `<tr>
+  <td> <input placeholder="ID" class="set_id" type="text">
+  </td>
+  <td><a class="btn-floating btn-small waves-effect waves-light blue btn_getID"
+      title="Get ID"><i class="material-icons">edit</i></a></td>
+  <td>
+    <input class="set_value" type="text">
+  </td>
+  <td><label>
+      <input type="checkbox" class="filled-in switch_checkbox" />      
+    </label></td>
+    <td><a class="deleteRow btn-floating btn-small waves-effect waves-light red"><i
+											class="material-icons">delete</i></a></td>
+								</td>
+</tr>`;
+}
+
+function actionElement(user) {
+	return /*html*/ `<div class="row user_${user} saveTable">
+          <div class="col s12">
+            <div>
+              <p class="showHideMenu" style="width: 100%; background: #64b5f6"><i class="material-icons">chevron_right</i>GetState</p>
+              <table style="display: none;" class="text-small">
+                <thead>
+                  <tr>
+                    <th style="width: 10%">Trigger</th>                      
+                    <th data-name="id" data-type="string" style="width: 25%;">
+                      ID</th>
+                    <th data-name="value" data-type="string" >
+                      Text</th>
+                    <th data-name="edit" style="width: 2%;">Newline
+                    </th>
+                    <th style="width: 2%"></th>
+                    <th data-name="delete" style="width: 2%;">
+                    </th>
+                  </tr>
+                </thead>
+                <tbody name=${user} data-name="get" class="table_get table-lines table-values">                
+                </tbody>
+              </table>
+            </div>
+            <div >
+            <p class="showHideMenu" style="width: 100%; background: #64b5f6"><i class="material-icons">chevron_right</i>SetState</p>
+              <table  style="display: none;" class="text-small">
+                <thead>
+                  <tr>
+                  <th data-name="trigger" style="width: 10%">Trigger</th>    
+                    <th data-name="id" data-type="string" style="width: 25%;">
+                      ID</th>
+                    <th data-name="value" data-type="string" >
+                      Value</th>
+                    <th data-name="switch" data-type="string" style="width: 5%;">
+                      Switch</th>
+                    <th data-name="edit" style="width: 2%;">
+                    </th>
+                    <th data-name="delete" style="width: 2%;">
+                    </th>
+                  </tr>
+                </thead>
+                <tbody name=${user} data-name="set" class="table_set table-lines table-values">                
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>`;
+}
+function newTableRow_Action(action, result) {
+	if (action === "get") {
+		return /*html*/ `<tr>
+    <td data-name="trigger">${result.trigger}</td>    
+    <td>${insertVal(result, "IDs")}</td>
+    <td>${insertVal(result, "text")}</td>
+    <td>${insertVal(result, "checkboxes")}</td>    
+    ${actionDeleteButton}
+    ${editButton}
+            </tr>`;
+	}
+	if (action === "set")
+		return /*html*/ `<tr>
+    <td data-name="trigger">${result.trigger}</td>
+    <td>${insertVal(result, "IDs")}</td>
+    <td>${insertVal(result, "values")}</td>
+    <td>${insertVal(result, "checkboxes")}</td>
+    ${actionDeleteButton}
+    ${editButton}
+            </tr>`;
+}
+const actionDeleteButton = `<td style="width: 5%;"><a class="btn-floating btn-small waves-effect waves-light red"><i	class="material-icons">delete</i></a></td>`;
+const editButton = `<td><a class="btn-floating btn-small waves-effect waves-light green"><i class="material-icons">edit</i></a></td>`;
+
+function insertVal(result, entry) {
+	let newEntry = "";
+	result[entry].forEach(function (element) {
+		newEntry += /*html*/ `<p class="value" data-name=${entry}> ${element}</p>`;
+	});
+	return newEntry;
+}
--- admin/js/main.js
+++ admin/js/main.js
@@ -7,7 +7,6 @@ function isStringEmty(classes) {
 	let allOk;
 	$(classes).each(function (key, element) {
 		if (element.value == "") {
-			console.log(this);
 			$(element).parent().addClass("bg-error");
 			allOk = false;
 		} else {
@@ -40,10 +39,6 @@ function generateNav() {
 		});
 		navigationArray[element] = { nav: [arrayNew] };
 		return navigationArray;
-
-		// sendTo(instance, "send", navigationArray, function (result) {
-		// 	console.log(result)
-		// })
 	});
 }
 /**
@@ -54,40 +49,76 @@ function generateNav() {
 function createUser(id, users) {
 	users.forEach((user) => {
 		$(id).append(newUserBtn(user));
-		createNav("#table_nav", user);
+		$("#table_nav").append(navElement(user));
+		$("#tab-action").append(actionElement(user));
 	});
 }
-function createNav(id, user) {
-	$(id).append(navElement(user));
-}
-function table2Values(id) {
-	const $div = $(id);
-	const $tbodys = $div.find("tbody");
-	const object = {};
 
+function table2Values(id) {
+	const $tbodys = $(id).find("tbody");
+	const object = { nav: {}, action: {} };
 	$tbodys.each(function () {
-		const nav = [];
+		const nav = [],
+			action = [];
 		const $tbody = $(this);
 		const $trs = $tbody.find("tr");
-		let user;
+		let saveName;
 		$trs.each(function () {
-			user = $tbody.attr("id");
-			const call = $(this).find("td input[data-name='call']").val();
-			const value = $(this).find("td input[data-name='value']").val();
-			const text = $(this).find("td input[data-name='text']").val();
-			let radio;
-			if ($(this).find("td input.nav-radio:radio:checked").val()) radio = true;
-			else radio = false;
+			saveName = $tbody.attr("name");
+			const dataName = $tbody.attr("data-name");
+			if (dataName === "nav") {
+				nav.push({
+					call: $(this).find("td input[data-name='call']").val(),
+					value: $(this).find("td input[data-name='value']").val(),
+					text: $(this).find("td input[data-name='text']").val(),
+					radio: $(this).find("td input.nav-radio").is(":checked"),
+				});
+				object.nav[saveName] = nav;
+			}
+			let id = [];
 
-			nav.push({ call: call, value: value, text: text, radio: radio });
+			//TODO - Function Global machen
+			$(this)
+				.find("p[data-name='IDs']")
+				.each(function () {
+					id.push($(this).html());
+				});
+			console.log(id);
+			if (dataName === "set" || dataName === "get")
+				action.push({
+					id: $(this).find("p[data-name='IDs']").html(),
+					checkbox: $(this).find("p[data-name='checkboxes']").is(":checked"),
+					trigger: $(this).find("td[data-name='trigger']").html(),
+				});
+			if (dataName === "set") {
+				action[0].value = $(this).find("p[data-name='values']").html();
+				object.action[saveName] = { set: action };
+			}
+			if (dataName === "get") {
+				action[0].text = $(this).find("p[data-name='text']").html();
+				object.action[saveName] = { get: action };
+			}
 		});
-		object[user] = { nav: nav };
+
+		if (action.length > 1) {
+		}
 	});
 	return object;
 }
-function showHideNav(activeUser) {
-	$("#navigation tbody").hide();
-	$(`tbody#${activeUser}`).show();
+function dataToArray(_this, selector) {
+	let val = [];
+	$(_this)
+		.find(selector)
+		.each(function () {
+			val.push($(this).html());
+		});
+	return val;
+}
+function showHideUserEntry(activeUser) {
+	$("tbody.table_switch_user").hide();
+	$("#tab-action>div").hide();
+	$(`tbody.table_switch_user.user_${activeUser}`).show();
+	$(`#tab-action>div.user_${activeUser}`).show();
 }
 /**
  *
@@ -101,4 +132,43 @@ function setCheckbox(checkbox) {
 			} else $(`#${id}`).prop("checked", false);
 		}
 	});
+}
+
+function splitTextInArray(activeUser) {
+	let value_list = [];
+	$(`#${activeUser} input[data-name="value"]`).each(function () {
+		let value = $(this).val();
+		if (typeof value == "string") {
+			value = value.replace(/&&/g, ",");
+			let array = value.split(",");
+			array.forEach((element) => {
+				value_list.push(element.trim());
+			});
+		}
+	});
+	return value_list;
+}
+function generateSelectTrigger(activeUser) {
+	let list = splitTextInArray(activeUser);
+	// HTML Elemente löschen und neu aufbauen
+	$("#select_trigger").empty().append(createSelectTrigger(list));
+}
+function fillTable(id, data, newTableRow_Nav, users) {
+	if (data) {
+		for (const name in data) {
+			const nav = data[name];
+			nav.forEach(function (element, key) {
+				$(`#${name}`).append(newTableRow_Nav(name, users));
+				if (element.call) $(`#${name} tr input.nav-call`)[key].value = element.call;
+				if (element.value) $(`#${name} tr input.nav-value`)[key].value = element.value;
+				if (element.text) $(`#${name}  tr input.nav-text`)[key].value = element.text;
+				if (element.radio) $(`#${name} tr input.nav-radio:radio`)[key].checked = element.radio;
+			});
+		}
+	}
+}
+
+//TODO - Anpassen
+function generatActionRow(user, action, result) {
+	$(`.user_${user} .table_${action}`).append(newTableRow_Action(action, result));
 }
