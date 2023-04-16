2f367529e4872e5ad8abfdcb7ec0400be5def46d
MiRo1310 <michael.roling@gmx.de>
2023-04-16 15:54:22 +0200

    eslint

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,5 +1,5 @@
 /*global newUserBtn,navElement ,actionElement,createSelectTrigger,newTableRow_Action,newTableRow_Action,newTrInAction, $*/
-/*eslint no-unused-vars: ["error", { "varsIgnorePattern": "isStringEmty|generateNav|create|set|fill|reset|add|show"}]*/
+/*eslint no-unused-vars: ["error", { "varsIgnorePattern": "isStringEmty|generate|create|set|fill|reset|add|show|ins|table"}]*/
 /**
  *
  * @param {string} classes Class to browse for empty String
