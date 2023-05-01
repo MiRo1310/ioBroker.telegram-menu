815ebf73a78f02d3efabad2cb8690a90559787b2
MiRo1310 <michael.roling@gmx.de>
2023-05-01 11:46:30 +0200

    url fixed

--- main.js
+++ main.js
@@ -248,9 +248,9 @@ class TelegramMenu extends utils.Adapter {
 
 									part.sendPic.forEach((element) => {
 										this.log.debug("elelmet " + JSON.stringify(element));
-										if (!token) token = "";
+										if (token.length > 0) token += " ";
 										exec(
-											`curl -H "Authorisation: Bearer" "${token} ${element.id}" > ${directoryPicture}`,
+											`curl -H "Authorisation: Bearer" "${token}${element.id}" > ${directoryPicture}`,
 										);
 										timeoutKey += 1;
 										const timeout = setTimeout(async () => {
