ee19c25a1cecb9408b6adba5c28659f28e9a35cd
MiRo1310 <michael.roling@gmx.de>
2023-07-17 18:44:58 +0200

    submenu

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+import { specialMenu } from "./lib/js/subMenu";
+import { sendToTelegramSubmenu } from "./lib/js/telegram";
+
 /*
  * Created with @iobroker/create-adapter v2.3.0
  */
@@ -221,6 +224,11 @@ class TelegramMenu extends utils.Adapter {
 											this.log.error("Error :" + JSON.stringify(e));
 										}
 									}
+								} else if (toDo == "menu") {
+									//TODO -
+									let newData = specialMenu(toDo);
+									if (newData && newData[0])
+										sendToTelegramSubmenu(this, userToSend, newData[0], newData[1]);
 								} else {
 									if (typeof userToSend == "string")
 										sendToTelegram(this, userToSend, textNoEntryFound);
@@ -287,28 +295,6 @@ class TelegramMenu extends utils.Adapter {
 			}
 		});
 
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		// await this.setObjectNotExistsAsync("testVariable", {
-		// 	type: "state",
-		// 	common: {
-		// 		name: "testVariable",
-		// 		type: "boolean",
-		// 		role: "indicator",
-		// 		read: true,
-		// 		write: true,
-		// 	},
-		// 	native: {},
-		// });
-
-		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		// this.subscribeStates("testVariable");
-		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-		// this.subscribeStates("lights.*");
-		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
 		/**
 		 *
 		 * @param {Array} array
@@ -323,27 +309,6 @@ class TelegramMenu extends utils.Adapter {
 		this.subscribeForeignStatesAsync("telegram.0.communicate.request");
 		this.subscribeForeignStatesAsync(telegramID);
 		this.subscribeForeignStatesAsync(`${instanceTelegram}.info.connection`);
-
-		/*
-			setState examples
-			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		// await this.setStateAsync("testVariable", true);
-
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		// 'await this.setStateAsync("testVariable", { val: true, ack: true });
-
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		// 'await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
-
-		// examples for the checkPassword/checkGroup functions
-		// let result = await this.checkPasswordAsync("admin", "iobroker");
-		// this.log.info("check user admin pw iobroker: " + result);
-
-		// result = await this.checkGroupAsync("admin", "admin");
-		// this.log.info("check group user admin group admin: " + result);
 	}
 
 	/**
@@ -363,45 +328,6 @@ class TelegramMenu extends utils.Adapter {
 		}
 	}
 
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  * @param {string} id
-	//  * @param {ioBroker.Object | null | undefined} obj
-	//  */
-	// onObjectChange(id, obj) {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
-
-	// /**
-	//  * Is called if a subscribed state changes
-	//  * @param {string} id
-	//  * @param {ioBroker.State | null | undefined} state
-	//  */
-	// onStateChange(id, state) {
-	// 	if (state) {
-
-	// 		// The state was changed
-	// 		this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-	// 	} else {
-	// 		// The state was deleted
-	// 		this.log.info(`state ${id} deleted`);
-	// 	}
-	// }
-
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
 	onMessage(obj) {
 		this.log.debug(obj);
 		if (typeof obj === "object" && obj.message) {
