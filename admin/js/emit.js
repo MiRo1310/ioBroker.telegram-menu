e5de8e5b5113805d7cf25d330d2d3d60ea71fe5f
MiRo1310 <michael.roling@gmx.de>
2023-08-13 09:00:09 +0200

    emit in sepearte js verschoben

---
+++ admin/js/emit.js
@@ -0,0 +1,58 @@
+function getUsersFromTelegram(socket, _this, telegramInstance) {
+	try {
+		socket.emit("getState", telegramInstance + ".communicate.users", (err, state) => {
+			if (state && !err) {
+				const usersInTelegram = JSON.parse(state.val);
+				const userListe = [];
+				for (const user in usersInTelegram) {
+					userListe.push(usersInTelegram[user]["firstName"]);
+				}
+				return userListe;
+			} else if (err) _this.log.debug("Error get Users vom Telegram: " + JSON.stringify(err));
+		});
+	} catch (err) {
+		_this.log.debug("Error get Users vom Telegram: " + JSON.stringify(err));
+	}
+}
+
+/**
+ *
+ * @param {*} socket
+ * @returns
+ */
+// @ts-ignore
+function getAllTelegramInstances(socket, _this) {
+	const id = [];
+	try {
+		socket.emit(
+			"getObjectView",
+			"system",
+			"instance",
+			{ startkey: "system.adapter.", endkey: "system.adapter.\u9999" },
+			function (err, doc) {
+				if (!err && doc.rows.length) {
+					for (let i = 0; i < doc.rows.length; i++) {
+						if (
+							doc.rows[i].value &&
+							doc.rows[i].value.common &&
+							doc.rows[i].value.common.titleLang &&
+							doc.rows[i].value.common.titleLang.en &&
+							doc.rows[i].value.common.titleLang.en == "Telegram"
+							// doc.rows[i].value.common.title == "Telegram"
+						) {
+							id.push(doc.rows[i].id.replace(/^system\.adapter\./, ""));
+						}
+						if (i == doc.rows.length - 1) {
+							id.forEach((id) => {
+								// @ts-ignore
+								$("#select_instance").append(newSelectInstanceRow(id));
+							});
+						}
+					}
+				} else if (err) _this.log.debug("Error all Telegram Users: " + JSON.stringify(err));
+			},
+		);
+	} catch (err) {
+		_this.log.debug("Error getAllTelegramInstance: " + JSON.stringify(err));
+	}
+}
--- admin/js/main.js
+++ admin/js/main.js
@@ -453,47 +453,6 @@ function generateStartside(users) {
 	return obj;
 }
 
-/**
- *
- * @param {*} socket
- * @returns
- */
-// @ts-ignore
-function getAllTelegramInstances(socket, _this) {
-	const id = [];
-	try {
-		socket.emit(
-			"getObjectView",
-			"system",
-			"instance",
-			{ startkey: "system.adapter.", endkey: "system.adapter.\u9999" },
-			function (err, doc) {
-				if (!err && doc.rows.length) {
-					for (let i = 0; i < doc.rows.length; i++) {
-						if (
-							doc.rows[i].value &&
-							doc.rows[i].value.common &&
-							doc.rows[i].value.common.titleLang &&
-							doc.rows[i].value.common.titleLang.en &&
-							doc.rows[i].value.common.titleLang.en == "Telegram"
-							// doc.rows[i].value.common.title == "Telegram"
-						) {
-							id.push(doc.rows[i].id.replace(/^system\.adapter\./, ""));
-						}
-						if (i == doc.rows.length - 1) {
-							id.forEach((id) => {
-								// @ts-ignore
-								$("#select_instance").append(newSelectInstanceRow(id));
-							});
-						}
-					}
-				}
-			},
-		);
-	} catch (err) {
-		_this.log.debug("Error getAllTelegramInstance: " + JSON.stringify(err));
-	}
-}
 // @ts-ignore
 function menuToSpan(activeGroup) {
 	$("#activMenuOutput").text(activeGroup);
