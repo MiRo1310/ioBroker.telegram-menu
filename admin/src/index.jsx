7d27b949d6f37f342adb8535fb0430c55d86583f
MiRo1310 <michael.roling@gmx.de>
2023-11-05 15:57:26 +0100

    theme color

--- admin/src/index.jsx
+++ admin/src/index.jsx
@@ -2,18 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import theme from "@iobroker/adapter-react-v5/Theme";
-import Utils from "@iobroker/adapter-react-v5/Components/Utils";
+
 import App from "./app";
 
-let themeName = Utils.getThemeName();
-
+let themeName = window.localStorage ? window.localStorage.getItem("App.theme") || "light" : "light";
+console.log(themeName);
 function build() {
-	ReactDOM.render(
+	return ReactDOM.render(
 		<MuiThemeProvider theme={theme(themeName)}>
-			<App />
+			<App
+				onThemeChange={(_theme) => {
+					themeName = _theme;
+					build();
+				}}
+				themeName={themeName}
+			/>
 		</MuiThemeProvider>,
 		document.getElementById("root"),
 	);
 }
+// function build() {
+// 	ReactDOM.render(
+// 		<MuiThemeProvider theme={theme(themeName)}>
+// 			<App />
+// 		</MuiThemeProvider>,
+// 		document.getElementById("root"),
+// 	);
+// }
 
 build();
