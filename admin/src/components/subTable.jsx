32989872d11c7580fa87cdc89ea4fec37409b40c
MiRo1310 <michael.roling@gmx.de>
2023-11-20 14:54:40 +0100

    Icons for true and false

--- admin/src/components/subTable.jsx
+++ admin/src/components/subTable.jsx
@@ -2,13 +2,24 @@ import React, { Component } from "react";
 import { Table, TableBody, TableCell, TableRow } from "@mui/material";
 
 class SubTable extends Component {
+	getElementIcon = (element) => {
+		const valtrue = "✔️";
+		const valfalse = "❌";
+		if (element === "true") {
+			return valtrue;
+		} else if (element === "false") {
+			return valfalse;
+		} else {
+			return element;
+		}
+	};
 	render() {
 		return (
 			<Table>
 				<TableBody>
 					{this.props.data.map((element, index) => (
 						<TableRow key={index} className="SubTable">
-							<TableCell style={{ padding: "0", border: "none" }}>{element}</TableCell>
+							<TableCell style={{ padding: "0", border: "none" }}>{this.getElementIcon(element)}</TableCell>
 						</TableRow>
 					))}
 				</TableBody>
