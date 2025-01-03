6269bf3e13ccb439d3e776665e2747aa610b98ca
MiRo1310 <michael.roling@gmx.de>
2025-01-03 09:34:58 +0100

    FEAT: #237 jump in nav

--- admin/src/pages/AppContentTabNavigationTableBodyValueModifier.tsx
+++ admin/src/pages/AppContentTabNavigationTableBodyValueModifier.tsx
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import type { RowForButton, TabValueEntries } from '@/types/app';
-import { getElementIcon } from '@/lib/actionUtils';
-import { splitTrimAndJoin } from '@/lib/string';
+import React, {Component} from 'react';
+import {CallbackFunctionsApp, DataMainContent, RowForButton, TabValueEntries} from '@/types/app';
+import {getElementIcon} from '@/lib/actionUtils';
+import {splitTrimAndJoin} from '@/lib/string';
 
 interface Props {
     row: RowForButton;
     entry: TabValueEntries;
+    data: DataMainContent & { entries: TabValueEntries[] };
+    callback: CallbackFunctionsApp;
 }
 
 class AppContentTabNavigationTableBodyValueModifier extends Component<Props> {
@@ -28,6 +30,28 @@ class AppContentTabNavigationTableBodyValueModifier extends Component<Props> {
         return this.props.entry.name === 'value';
     }
 
+    findMenuInNav = (button: string): string | undefined => {
+        const nav = this.props.data.state.native.data.nav
+        console.log(nav)
+        let menu = ""
+        const menuKeyValue = Object.entries(nav).find(([key, value]) => value.find((element) => element.call === button))
+
+        if (!menuKeyValue) return
+        menu = menuKeyValue[0]
+
+        console.log(menuKeyValue)
+        return menu
+    }
+
+    buttonClick = (button: string) => {
+        console.log(button)
+        const menu = this.findMenuInNav(button);
+        if (menu) {
+            this.props.callback.setStateApp({activeMenu: menu})
+        }
+    }
+
+
     render(): React.ReactNode {
         return (
             <>
@@ -36,13 +60,14 @@ class AppContentTabNavigationTableBodyValueModifier extends Component<Props> {
                         {this.getValue().map((row, i) =>
                             row !== '' ? (
                                 <p
-                                    className={'nav__row'}
+                                    className={'nav__row noneDraggable'}
                                     key={i}
                                 >
-                                    {(!row.includes('menu:') ? row.split(',') : [row]).map((button, index) => (
+                                    {(!row.includes('menu:') ? row.split(',') : [row]).map((button) => button.trim()).map((button, index) => (
                                         <span
-                                            className={`row__button ${button.includes('menu:') ? 'row__submenu' : ''}`}
+                                            className={`row__button cursor-pointer ${button.includes('menu:') ? 'row__submenu' : ''}`}
                                             key={index}
+                                            onClick={() => this.buttonClick(button)}
                                         >
                                             {button}{' '}
                                         </span>
