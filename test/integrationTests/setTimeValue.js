06ea182f2812de1981ee5f7070582a68d13fe957
MiRo1310 <michael.roling@gmx.de>
2025-09-28 08:22:47 +0200

    Test

--- test/integrationTests/setTimeValue.js
+++ test/integrationTests/setTimeValue.js
@@ -3,30 +3,25 @@
 const expect = require('chai').expect;
 
 exports.runTests = function (suite) {
-    suite('alarm_control_panel', getHarness => {
+    suite('Replace time', getHarness => {
         let harness;
+        let setTimeValue;
 
         before(async () => {
             harness = getHarness();
+            console.log('Inhalt von __dirname:', require('fs').readdirSync(__dirname + '/../../src/lib/'));
+            const utils = require('../../src/lib/utilities.ts');
+            setTimeValue = utils.setTimeValue;
         });
 
         it('should replace Zeit korrekt bei gültiger ID', async () => {
             // Mock für adapter.getForeignStateAsync
             harness.objects.getForeignStateAsync = async () => ({ ts: 1710000000000, lc: 1710000000000 });
-            const { setTimeValue } = require('../../build/lib/utilities');
-            const text = "Text {time.lc,(DD MM YYYY hh:mm:ss:sss),id:'testId'}";
-            const result = await setTimeValue(text);
-            expect(result).to.not.include('Invalid ID');
-            expect(result).to.not.equal('Text 2025 10 08 03:20:00:000');
+            await harness.startAdapterAndWait();
+            console.log('import setTimeValue');
+            // const text = "Text {time.lc,(DD MM YYYY hh:mm:ss:sss),id:'testId'}";
+            // await setTimeValue(text);
+            expect(true).to.false;
         });
-
-        // it('should return "Invalid ID" bei ungültiger ID', async () => {
-        //     harness.adapter.getForeignStateAsync = async () => undefined;
-        //
-        //     const { setTimeValue } = require('../../src/lib/utilities');
-        //     const text = "Text {time.lc,(DD MM YYYY hh:mm:ss:sss),id:'bad'}";
-        //     const result = await setTimeValue(text);
-        //     expect(result).to.include('Invalid ID');
-        // });
     });
 };
