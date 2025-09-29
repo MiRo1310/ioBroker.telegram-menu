f466e27f8b7df29cd34e9b094152b4a88eaadf9c
MiRo1310 <michael.roling@gmx.de>
2025-09-29 19:51:13 +0200

    Test

--- test/integration.js
+++ test/integration.js
@@ -1,5 +1,11 @@
 const path = require('path');
 const { tests } = require('@iobroker/testing');
+const { TestContext } = require('@iobroker/testing/build/tests/integration');
+const integrationTests = require('./integrationTests/setTimeValue');
 
 // Run tests
-tests.integration(path.join(__dirname, '..'));
+tests.integration(path.join(__dirname, '..'), {
+    defineAdditionalTests({ suite }) {
+        integrationTests.runTests(suite);
+    },
+});
--- test/integrationTests/setTimeValue.ts
+++ test/integrationTests/setTimeValue.ts
@@ -6,7 +6,7 @@ import { TestSuite } from '@iobroker/testing/build/tests/integration';
 import { TestHarness } from '@iobroker/testing/build/tests/integration/lib/harness';
 
 const runTests = function (suite: TestSuite) {
-    suite('Replace time', getHarness => {
+    suite('Test', getHarness => {
         let harness: TestHarness;
 
         before(async () => {
@@ -15,17 +15,11 @@ const runTests = function (suite: TestSuite) {
             await harness.startAdapterAndWait();
         });
 
-        it('should replace Zeit korrekt bei gültiger ID', async () => {
-            // Mock für adapter.getForeignStateAsync
-            // harness.objects.getForeignStateAsync = async () => ({ ts: 1710000000000, lc: 1710000000000 });
-            if (harness.isControllerRunning()) {
-                console.log('import setTimeValue');
-                const text = "Text {time.lc,(DD MM YYYY hh:mm:ss:sss),id:'testId'}";
-                await setTimeValue(text);
-                expect(true).to.false;
-            } else {
-                console.log('Controller not running');
-            }
+        it('Test', async () => {
+            harness.objects.getForeignStateAsync = async () => ({ ts: 1710000000000, lc: 1710000000000 });
+            const text = "Text {time.lc,(DD MM YYYY hh:mm:ss:sss),id:'testId'}";
+            await setTimeValue(text);
+            expect(true).to.false;
         });
     });
 };
