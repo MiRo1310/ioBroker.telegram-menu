879635e2df1d68b56d2f6cbed2208e42a611b842
MiRo1310 <michael.roling@gmx.de>
2025-10-03 08:25:46 +0200

    remove last

--- test/integration/setTimeValue.ts
+++ test/integration/setTimeValue.ts
@@ -15,7 +15,7 @@ export default function runTests(suite: TestSuite) {
         });
 
         it('Test', async () => {
-            harness.objects.getForeignStateAsync = async () => ({ ts: 1710000000000, lc: 1710000000000 });
+            // harness.objects.getForeignStateAsync = async () => ({ ts: 1710000000000, lc: 1710000000000 });
             const text = "Text {time.lc,(DD MM YYYY hh:mm:ss:sss),id:'testId'}";
             await setTimeValue(text);
             expect(true).to.false;
