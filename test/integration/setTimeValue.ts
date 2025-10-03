2021c0a42a46618466a95d61db13dd832e27ce18
MiRo1310 <michael.roling@gmx.de>
2025-10-03 08:04:58 +0200

    Add startController

--- test/integration/setTimeValue.ts
+++ test/integration/setTimeValue.ts
@@ -10,6 +10,7 @@ export default function runTests(suite: TestSuite) {
 
         before(async () => {
             harness = getHarness();
+            await harness.startController();
         });
 
         it('Test', async () => {
