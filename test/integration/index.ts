331a3f20e399bf869af06dce3efaedfc1211822e
MiRo1310 <michael.roling@gmx.de>
2025-10-09 16:05:02 +0200

    Remove Integration Tests

--- test/integration/index.ts
+++
@@ -1,7 +0,0 @@
-import { TestSuite } from '@iobroker/testing/build/tests/integration';
-
-import runTests from './setTimeValue';
-
-export default function tests(suite: TestSuite) {
-    runTests(suite);
-}
