00aafd238bf51f7539ec6e789db079aad8183152
MiRo1310 <michael.roling@gmx.de>
2026-04-19 09:14:35 +0200

    test: Add unit tests for Test2 functionality

--- build/app/test.js
+++ build/app/test.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Test = void 0;
+exports.Test2 = exports.Test = void 0;
 const Test = () => {
     console.log('Test');
 };
 exports.Test = Test;
+const Test2 = () => {
+    console.log('Test');
+};
+exports.Test2 = Test2;
 //# sourceMappingURL=test.js.map
