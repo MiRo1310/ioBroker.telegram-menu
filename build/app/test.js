b592b01d05f7bd6b3141ebeca4b8ba576635f24f
MiRo1310 <michael.roling@gmx.de>
2026-04-19 09:11:31 +0200

    test

---
+++ build/app/test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Test = void 0;
+const Test = () => {
+    console.log('Test');
+};
+exports.Test = Test;
+//# sourceMappingURL=test.js.map
---
+++ src/app/test.ts
@@ -0,0 +1,3 @@
+export const Test = (): void => {
+    console.log('Test');
+};
