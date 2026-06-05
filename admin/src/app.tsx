0548bdcfa71ca0edd2899ab8c0e72f316d5c6683
MiRo1310 <michael.roling@gmx.de>
2026-06-05 17:17:48 +0200

    fix:#555 prevent double sending of state IDs when confirm=true

--- admin/src/app.tsx
+++ admin/src/app.tsx
@@ -16,17 +16,17 @@ import { getDefaultDropBoxCoordinates } from './lib/dragNDrop';
 import { getDoubleEntries, getFirstItem as getFirstObjectKey } from './lib/object';
 import type { TelegramMenuApp } from './types/props-types';
 import { processUserData } from './lib/Utils';
-import en from '../i18n/en/translations.json';
-import de from '../i18n/de/translations.json';
-import ru from '../i18n/ru/translations.json';
-import pt from '../i18n/pt/translations.json';
-import nl from '../i18n/nl/translations.json';
-import fr from '../i18n/fr/translations.json';
-import it from '../i18n/it/translations.json';
-import es from '../i18n/es/translations.json';
-import pl from '../i18n/pl/translations.json';
-import uk from '../i18n/uk/translations.json';
-import zhCN from '../i18n/zh-cn/translations.json';
+import en from '../i18n/en.json';
+import de from '../i18n/de.json';
+import ru from '../i18n/ru.json';
+import pt from '../i18n/pt.json';
+import nl from '../i18n/nl.json';
+import fr from '../i18n/fr.json';
+import it from '../i18n/it.json';
+import es from '../i18n/es.json';
+import pl from '../i18n/pl.json';
+import uk from '../i18n/uk.json';
+import zhCN from '../i18n/zh-cn.json';
 
 class App extends GenericApp<TelegramMenuApp.AdditionalProps, TelegramMenuApp.AdditionalState> {
     dropBoxRef: Dropbox.Ref;
